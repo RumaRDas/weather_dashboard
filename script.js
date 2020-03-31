94f76138b8978794d4ff746039d7c17b7fdfdb52
Ruma Das <rumaadl16@gmail.com>
2020-03-31 19:57:01 +1030

    adding img

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
 
-function getweather(city){
+function getWeather(city) {
+
     // Querying the weather api for the selected city, the ?app_id parameter is required, but can equal anything
-    var queryURL="http://api.openweathermap.org/data/2.5/weather?q="+city+ "&units=imperial" + "&APPID=cd9a537cb0b8a066e19b9065531d35d3";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=cd9a537cb0b8a066e19b9065531d35d3";
     // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
-     // Printing the entire object to console
+    }).then(function (response) {
+        // Printing the entire object to console
         console.log(response);
-         // Constructing HTML containing the weather information
-         var cityName =$("<h2>").text(response.name);
-         var date = $("<h3>").text(response.dt*1000);
-         var temperature = $("<p>").text("Temperature: " +(response.main.temp_min) +"&deg;F");
-         var humiDity = $("<p>").text("Humidity:  " + ( response.main.humidity) +"%");
-         var windspeed = $("<p>").text("WindSpeed: " + (response.wind.speed) +"MPH");
-         var uv = $("<p>").text("WindSpeed: " + (response.wind.speed) +"MPH");
-        
-         $("#showweather").empty();
-         $("#showweather").append(cityName, date,temperature, humiDity,windspeed,uv);
-         
-
-      });
-
+        // Constructing HTML containing the weather information
+        var cityName = $("<h1>").text(response.name);
+        let date = new Date().toISOString().slice(0, 10);
+        var temperature = $("<p>").text("Temperature: " + (response.main.temp_min) + "F");
+        var humiDity = $("<p>").text("Humidity:  " + (response.main.humidity) + "%");
+        var windspeed = $("<p>").text("WindSpeed: " + (response.wind.speed) + "MPH");
+        var uv = $("<p>").text("WindSpeed: " + (response.wind.speed) + "MPH");
+        $("#showweather").empty();
+        $("#showweather").append(cityName, date, temperature, humiDity, windspeed, uv);
+        renderCities(response.name);
+    });
 }
-$("#seacrhbtn").click(function(event){ 
-    //prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
-    event.preventDefault();
-    var inputcity =$("#city-input").val().trim();
-   // cities.push(city);
-  //  searchcity();
-  getweather(inputcity);
 
+function renderCities(city) {
+    cityAdd = $("<div>");
+    cityAdd.text(city);
+    $("#cityList").append(cityAdd);
+}
+
+$("#searchbtn").click(function (event) {
+    //prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
+    var inputCity = $("#city-input").val().trim();
+    // cities.push(city);
+    //  searchcity();
+    getWeather(inputCity);
 });
 
+{
+// $("#seacrhbtn").click(function (event) {
+//     //prevents the form from trying to submit itself.
+//     // We're using a form so that the user can hit enter instead of clicking the button if they want
+//     event.preventDefault();
+//     var city = $("#city-input").val().trim();
+//     cities.push(city);
+//     searchcity();
+//     getweather(city);
+
+// });
 /*
 $(document).ready(function(){
-    $("#seacrhbtn").click(function(){ 
-     
+    $("#seacrhbtn").click(function(){
+
         return getweather()
 });
 });
@@ -70,3 +85,4 @@ function showResults(data){
     ;
 
 }*/
+}
