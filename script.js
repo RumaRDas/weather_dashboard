400aec2e586cd8cd1a51db878c221bcf45829a8d
Ruma Das <rumaadl16@gmail.com>
2020-03-31 20:44:55 +1030

    changing css js

--- script.js
+++ script.js
@@ -33,61 +33,13 @@ function renderCities(city) {
     cityAdd.text(city);
     $("#cityList").prepend(cityAdd);
 }
-
+//geting the weather of search cities
 $("#searchbtn").click(function (event) {
     //prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    $("#showweather").show();
     event.preventDefault();
     var inputCity = $("#city-input").val().trim();
-    // cities.push(city);
-    //  searchcity();
     getWeather(inputCity);
 });
 
-{
-// $("#seacrhbtn").click(function (event) {
-//     //prevents the form from trying to submit itself.
-//     // We're using a form so that the user can hit enter instead of clicking the button if they want
-//     event.preventDefault();
-//     var city = $("#city-input").val().trim();
-//     cities.push(city);
-//     searchcity();
-//     getweather(city);
 
-// });
-/*
-$(document).ready(function(){
-    $("#seacrhbtn").click(function(){
-
-        return getweather()
-});
-});
-function getweather(){
-    var city =$("#city-input").val();
-    if(city != ""){
-        $.ajax({
-            url:"http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=imperial" +"&APPID=cd9a537cb0b8a066e19b9065531d35d3",
-            type:"GET",
-            datatype: "jsonp",
-            success: function(data){
-              console.log (data);
-               var widget =showResults(data);
-              $("#showweather").html(widget);
-              $("#city").val('');
-            }
-        });
-    }else{
-        $("#error").html("<div> Give a City Name </div>");
-    }
-}
-function showResults(data){
-    return "<h3>"+data.name+"</h3>"+
-           "<h4> " + new Date(data.dt*1000).toISOString() + "</h4>: " +
-           "<p>Temperature:  "+ data.main.temp_min +" &deg;F </p>"+
-           "<p>Humidity:  "+ data.main.humidity +" % </p>"+
-           "<p>WindSpeed:  "+ data.wind.speed +"  MPH </p>"+
-           "<p>UV Index:  "+ data.main.temp +"  </p>"
-    ;
-
-}*/
-}
